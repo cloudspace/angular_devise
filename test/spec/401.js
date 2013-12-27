4a4778449fb8101ee3b5b6f220995e5c9cc9d64a
Justin Ridgewell <jridgewell@cloudspace.com>
2013-12-27 15:42:58 -0500

    Spec out deviseInterceptor401 behavior

--- test/spec/401.js
+++ test/spec/401.js
@@ -6,13 +6,42 @@ describe('Service: Devise.401', function () {
     beforeEach(module('Devise'));
 
     // instantiate service
-    var Auth;
-    beforeEach(inject(function (_Auth_) {
-        Auth = _Auth_;
+    var interceptor, promise, wrappedPromise;
+    var $http, $httpBackend;
+    beforeEach(inject(function (_deviseInterceptor401_) {
+        interceptor = _deviseInterceptor401_;
+        wrappedPromise = {};
+        promise = {
+            then: jasmine.createSpy('then').andReturn(wrappedPromise)
+        };
+    }));
+    beforeEach(inject(function(_$http_, _$httpBackend_) {
+        $http = _$http_;
+        $httpBackend = _$httpBackend_;
     }));
 
-    it('should do something', function () {
-        expect(!!Auth).toBe(true);
+    describe('responseError', function() {
+        beforeEach(function() {
+            interceptor = interceptor.responseError;
+            $httpBackend.expect('GET', '/foo').respond(401);
+        });
+
+        it('returns rejected promise on 401', function () {
+            var callback = jasmine.createSpy('callback');
+            $http.get('/foo').catch(function() {
+                callback();
+            });
+            $httpBackend.flush();
+            expect(callback).toHaveBeenCalled();
+        });
+
+        it('broadcasts "devise:unauthorized" on 401 error', inject(function ($rootScope) {
+            var callback = jasmine.createSpy('callback');
+            $rootScope.$on('devise:unauthorized', callback);
+            $http.get('/foo');
+            $httpBackend.flush();
+            expect(callback).toHaveBeenCalled();
+        }));
     });
 
 });
